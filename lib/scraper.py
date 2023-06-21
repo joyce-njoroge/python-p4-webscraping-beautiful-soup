b84c84fd862a729d7af8200149b80f17307fdb06
Joyce Njoroge <joycewambuku33@gmail.com>
2023-06-21 23:17:28 +0300

    complete

--- lib/scraper.py
+++ lib/scraper.py
@@ -3,4 +3,15 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+html = requests.get("https://Moringaschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+
+# print(doc)
+
+# print(doc.select('.heading-financier')[0].contents)
+#print(doc.select('.heading-60-black.color-black.mb-20'))
+
+courses = doc.select('.heading-60-black.color-black.mb-20')
+
+for course in courses: 
+  print(course.contents[0].strip())
